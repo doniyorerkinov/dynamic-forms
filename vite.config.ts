832eb33301be17b18172c000da0cf991fac13151
doniyorerkinov <erkinovdoniyor03@gmail.com>
2025-03-25 04:38:54 +0500

    vite

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { fileURLToPath } from 'url';
+import { fileURLToPath } from "url";
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
@@ -12,5 +12,11 @@ export default defineConfig({
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  server: {
+    port: 3000,
+  },
+  build: {
+    outDir: "dist",
   },
 });
